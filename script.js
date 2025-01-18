4e66bf0e00e334be0c63af6872ee2a71b65823e8
Simon Dahal <simon.dahal@coderacademy.edu.au>
2025-01-18 11:21:17 +1100

    functions

--- script.js
+++ script.js
@@ -180,6 +180,12 @@ fruits.forEach(function fruitLoop(fruit, index) {
     console.log(fruit, index)
 })
 
+// using arrow function
+console.log("forEach using arrow")
+fruits.forEach((fruit, index) => {
+    console.log(fruit, index)
+})
+
 // const fruits = ["Mango", "Orange", "Apple"]
 
 
@@ -189,6 +195,12 @@ const upperCaseFruits = fruits.map(function fruitLoop(fruit) {
 
 console.log(upperCaseFruits)
 
+// using arrow function
+const upperCaseFruitsUsingArrow = fruits.map(fruit => fruit.toUpperCase())
+
+console.log("map using arrow")
+console.log(upperCaseFruitsUsingArrow)
+
 
 const newFruits = ["Mango", "Orange", "Apple", "Banana"]
 
@@ -197,3 +209,97 @@ const filteredFruits = newFruits.filter(function filterLoop(fruit) {
 })
 
 console.log(filteredFruits)
+
+
+// Function
+
+function greet(name) {
+    console.log("Hello " + name)
+}
+
+greet("Jane")
+// name - parameter
+// "Jane" - argument
+
+
+// return
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+const totalSum = sum(5, 7)
+console.log(totalSum)
+
+
+// First class functions - first class citizens
+// can be assigned to a variable
+// can be passed as argument to a function
+// can be returned from a function
+
+// anonymous functions
+// const addFn = function(a, b) {
+//     return a + b
+// }
+
+// arrow function
+const addFn = (a, b) => {
+    return a + b
+}
+const total = addFn(1, 2)
+console.log(total)
+
+
+function player() {
+    console.log("Player")
+}
+
+function admin() {
+    console.log("Admin")
+}
+
+function greetUser(user) {
+    console.log("Hi")
+    user()
+}
+
+greetUser(player)
+greetUser(admin)
+
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function calculate(a, b, operation) {
+    return operation(a, b)
+}
+
+console.log(calculate(1, 2, add))
+console.log(calculate(7, 5, subtract))
+
+
+// Closure
+// In case of nested functions, the inner function will have access to the
+// value of variables of outer function even when the outer function
+// has finished executing
+function multiplier(factor) {
+    function product(num) {
+        return num * factor
+    }
+    return product
+}
+
+const doubler = multiplier(2)
+
+console.log(doubler(5))
+console.log(doubler(11))
+console.log(doubler(23))
+
+const tripler = multiplier(3)
+
+console.log(tripler(3))
+console.log(tripler(7))
